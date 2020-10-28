601e43b0cdd1eff298cdd9fd26fa2c64cfb3a293
tilmaz38 <abdullahyilmaz633@gmail.com>
2020-10-28 17:35:00 +0300

    added new files
    added new files

---
+++ framework/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+	// Ufak cihazlar için menu açma butonu >>>>
+	$('#acmakapamaheader').click(function () {
+		$('#headacmabutonusinir').slideToggle('slow');
+	});
+	// Ufak cihazlar için menu açma butonu <<<<<
+
+	// Main-Footer alanı için üstten boşuk hesaplamaları >>>>
+	// var dokumangenisligi = $(window).outerWidth();
+
+	// if (!$(document('headermesajalani').length)) {
+	// 	if (dokumangenisligi >= 1175) {
+	// 		var usttenbosluk = 188;
+	// 	} else if (dokumangenisligi >= 992 && dokumangenisligi < 1175) {
+	// 		var usttenbosluk = 98;
+	// 	} else if (dokumangenisligi >= 768 && dokumangenisligi < 992) {
+	// 		var usttenbosluk = 88;
+	// 	} else if (dokumangenisligi >= 600 && dokumangenisligi < 768) {
+	// 		var usttenbosluk = 788;
+	// 	} else if (dokumangenisligi < 600) {
+	// 		var usttenbosluk = 78;
+	// 	}
+	// } else {
+	// 	if (dokumangenisligi >= 1175) {
+	// 		var usttenbosluk = 148;
+	// 	} else if (dokumangenisligi >= 992 && dokumangenisligi < 1175) {
+	// 		var usttenbosluk = 138;
+	// 	} else if (dokumangenisligi >= 768 && dokumangenisligi < 992) {
+	// 		var usttenbosluk = 118;
+	// 	} else if (dokumangenisligi >= 600 && dokumangenisligi < 768) {
+	// 		var usttenbosluk = 108;
+	// 	} else if (dokumangenisligi < 600) {
+	// 		var usttenbosluk = 103;
+	// 	}
+	// }
+	// $('main').css('top', usttenbosluk);
+	// $('footer').css('top', usttenbosluk);
+	// $(window).resize(function () {
+	// 	var dokumangenisligi = $(window).outerWidth();
+
+	// 	if (!$(document('headermesajalani').length)) {
+	// 		if (dokumangenisligi >= 1175) {
+	// 			var usttenbosluk = 188;
+	// 		} else if (dokumangenisligi >= 992 && dokumangenisligi < 1175) {
+	// 			var usttenbosluk = 98;
+	// 		} else if (dokumangenisligi >= 768 && dokumangenisligi < 992) {
+	// 			var usttenbosluk = 88;
+	// 		} else if (dokumangenisligi >= 600 && dokumangenisligi < 768) {
+	// 			var usttenbosluk = 788;
+	// 		} else if (dokumangenisligi < 600) {
+	// 			var usttenbosluk = 78;
+	// 		}
+	// 	} else {
+	// 		if (dokumangenisligi >= 1175) {
+	// 			var usttenbosluk = 148;
+	// 		} else if (dokumangenisligi >= 992 && dokumangenisligi < 1175) {
+	// 			var usttenbosluk = 138;
+	// 		} else if (dokumangenisligi >= 768 && dokumangenisligi < 992) {
+	// 			var usttenbosluk = 118;
+	// 		} else if (dokumangenisligi >= 600 && dokumangenisligi < 768) {
+	// 			var usttenbosluk = 108;
+	// 		} else if (dokumangenisligi < 600) {
+	// 			var usttenbosluk = 103;
+	// 		}
+	// 	}
+	// });
+
+	// Main-Footer alanı için üstten boşuk hesaplamaları <<<<<
+});
